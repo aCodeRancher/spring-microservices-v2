b3ec8e16f044ed1656598fa91aadc01910f55942
helen <hhma@alumni.cpp.edu>
2021-02-22 13:08:51 -0500

    add a test case testing profile = qa

--- 03.microservices/limits-service/src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
+++ 03.microservices/limits-service/src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -4,24 +4,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class LimitsServiceApplicationTests {
+@TestPropertySource("/application-test.properties")
+public class LimitsServiceApplicationTests {
 
+    @Autowired
+    MockMvc mockmvc;
 
-	@Autowired
-	MockMvc mockmvc;
-
-	@Test
-	void testLimits() throws Exception {
-		mockmvc.perform(get("/limits"))
-				.andExpect(jsonPath("$.minimum").value("3"))
-				.andExpect(jsonPath("$.maximum").value("997"));
-	}
-
+    @Test
+    void testLimits() throws Exception {
+        mockmvc.perform(get("/limits"))
+                .andExpect(jsonPath("$.minimum").value("21"))
+                .andExpect(jsonPath("$.maximum").value("2222"));
+    }
 }
