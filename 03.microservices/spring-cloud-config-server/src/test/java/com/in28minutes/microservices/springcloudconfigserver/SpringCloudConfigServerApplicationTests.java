1769338487eaee46b82537500cb68d0c6c04274b
helen <hhma@alumni.cpp.edu>
2021-02-23 11:21:19 -0500

    add spring boot test for Spring cloud config server itself.

--- 03.microservices/spring-cloud-config-server/src/test/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
+++ 03.microservices/spring-cloud-config-server/src/test/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -1,13 +1,39 @@
 package com.in28minutes.microservices.springcloudconfigserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringCloudConfigServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	MockMvc mockMvc;
 
+	@Test
+	void testQA() throws Exception {
+		//The default profile values are included
+		mockMvc.perform(get("/limits-service/qa"))
+				.andExpect(jsonPath("$.profiles.[0]").value("qa"))
+		        .andExpect(jsonPath("$.propertySources.[0].source.['limits-service.minimum']").value("21"))
+				.andExpect(jsonPath("$.propertySources.[0].source.['limits-service.maximum']").value("2222"))
+				.andExpect(jsonPath("$.propertySources.[1].source.['limits-service.minimum']").value("1"))
+				.andExpect(jsonPath("$.propertySources.[1].source.['limits-service.maximum']").value("1111"));
+    }
+
+	@Test
+	void testDev() throws Exception {
+		//The default profile values are included
+		mockMvc.perform(get("/limits-service/dev"))
+				.andExpect(jsonPath("$.profiles.[0]").value("dev"))
+				.andExpect(jsonPath("$.propertySources.[0].source.['limits-service.minimum']").value("1222"))
+				.andExpect(jsonPath("$.propertySources.[0].source.['limits-service.maximum']").value("9999"))
+				.andExpect(jsonPath("$.propertySources.[1].source.['limits-service.minimum']").value("1"))
+				.andExpect(jsonPath("$.propertySources.[1].source.['limits-service.maximum']").value("1111"));
+	}
 }
