360344d0894330b9c51cb876f74d50fefdc6d12a
helen <hhma@alumni.cpp.edu>
2021-04-12 11:13:24 -0400

    add @EnableCircuitBreaker

--- 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.in28minutes.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
