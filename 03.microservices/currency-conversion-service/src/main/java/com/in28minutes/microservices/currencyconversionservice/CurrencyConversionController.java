e4f584f2eab00e327b01a7e7d604443a9443dbc9
helen <hhma@alumni.cpp.edu>
2021-04-12 11:12:49 -0400

    add fallback method

--- 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ 03.microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,8 +3,13 @@ package com.in28minutes.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +20,12 @@ public class CurrencyConversionController {
 	
 	@Autowired
 	private CurrencyExchangeProxy proxy;
-	
+
+	Logger logger = LoggerFactory.getLogger(CurrencyConversion.class);
+
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	//@CircuitBreaker(name="currency-exchange", fallbackMethod= "retryFallback")
+	@HystrixCommand(fallbackMethod="retryFallback")
 	public CurrencyConversion calculateCurrencyConversion(
 			@PathVariable String from,
 			@PathVariable String to,
@@ -41,6 +50,18 @@ public class CurrencyConversionController {
 		
 	}
 
+	public CurrencyConversion retryFallback(String from, String to, BigDecimal quantity){
+		logger.info("fallback method called");
+		CurrencyConversion cc = new CurrencyConversion();
+		cc.setId(9000l);
+		cc.setFrom(from);
+		cc.setTo(to);
+		cc.setConversionMultiple(BigDecimal.valueOf(1));
+		cc.setEnvironment("8999");
+		cc.setQuantity(quantity);
+		cc.setTotalCalculatedAmount(BigDecimal.valueOf(1l));
+		return cc;
+	}
 	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversionFeign(
 			@PathVariable String from,
